3fe64b64b12ed05dde8d95ee87b477cd354d7406
Muhammad khubaib <120232076+MKQazi786@users.noreply.github.com>
2023-06-13 17:38:05 +0500

    i have added 2 tasks in my web

--- 9 tasks.js
+++ 9 tasks.js
@@ -1,8 +1,25 @@
-window.Factorial= (event) => { {
-    event.preventDefault()
+window.Factorial = (event) => {
+        event.preventDefault();
+
+   let userinput1 = parseInt(document.getElementById("task 1").value);
+
+    let result = 1
+
+    for (let i = 1; i <= userinput1; i++) {
+        result *= i;
+
+    }
+
+    document.getElementById("result1").innerHTML = `The Factorial Of <b>'${userinput1}'</b> is <b>'${result}'</b>`;
 }
 
-   let userinput1 = document.getElementById("task 1").value;
+window.VowelsRemoving = (event) => {
+     event.preventDefault();
 
+    let userinput2 = document.getElementById("task 2").value;
+    userinput2= userinput2.toLowerCase();
+    
+    let result2 = userinput2.replace(/[aeiou]/g,'');
 
+    document.getElementById("result2").innerHTML = `your <b>'${userinput2}'</b> after removing of vowels is become this <b>'${result2}'</b>`;
 }
