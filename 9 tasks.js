900536a8b624b3d0456e70ba1c7fed393bf3bed7
Muhammad khubaib <120232076+MKQazi786@users.noreply.github.com>
2023-06-15 14:44:15 +0500

    i have change my repo because i have face error

--- 9 tasks.js
+++ 9 tasks.js
@@ -13,13 +13,38 @@ window.Factorial = (event) => {
     document.getElementById("result1").innerHTML = `The Factorial Of <b>'${userinput1}'</b> is <b>'${result}'</b>`;
 }
 
-window.VowelsRemoving = (event) => {
-     event.preventDefault();
+window.vowelsRemoving = (event) => {
+    event.preventDefault();
 
-    let userinput2 = document.getElementById("task 2").value;
-    userinput2= userinput2.toLowerCase();
-    
-    let result2 = userinput2.replace(/[aeiou]/g,'');
+    let userinput2 = document.getElementById("task 2").value
+
+    userinput2 = userinput2.toLowerCase();
+
+    let result2 = userinput2.replace(/[aeiou]/g,'')
 
     document.getElementById("result2").innerHTML = `your <b>'${userinput2}'</b> after removing of vowels is become this <b>'${result2}'</b>`;
+}
+
+window.largestNumberOnTop = (event) => {
+     event.preventDefault();
+
+    let userinput3 = document.getElementById("task 3").value;
+
+   let a = userinput3.split(",");
+   console.log(a)
+
+    let result3 = Math.max(...a)
+
+    console.log(result3)
+   
+    document.getElementById("result3").innerHTML = `your <b>'${userinput3}'</b> after set largest number on top is become this <b>'${result3}'</b>`;
+}
+
+window.capitalizedEachFirstLetter = (event) => {
+     event.preventDefault();
+
+    let userinput4 = document.getElementById("task 4").value;
+
+   
+    document.getElementById("result4").innerHTML = `your <b>'${userinput4}'</b> after each letter capitalized is become this <b>'${result4}'</b>`;
 }
